5ef8baa62685fa8efc8e69b7e2e9e8754c4b2a7e
= <=>
2023-07-12 12:46:57 +0200

    type+react = only structure, i need to list all todoes and css

--- toDoReact/to_do-app/src/components/buton.ts
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const Buton = () =>{
-    return(
-        <div>
-            <Button type={"submit"} variant={"contained"} className={classes.button}>Submit</Button>
-        </div>
-    );
-}
